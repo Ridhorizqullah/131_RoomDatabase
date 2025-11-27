2fc2b4533ecbd29eb0879131ab43639c20192238
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-27 16:20:35 +0700

    Membuat implementasi offline untuk RepositoriSiswa

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -2,13 +2,18 @@ package com.example.roomdatabase.repositori
 
 
 import com.example.roomdatabase.room.Siswa
+import com.example.roomdatabase.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 
-interface RepositoriSiswa{
+interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
-
 }
 
-class
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
