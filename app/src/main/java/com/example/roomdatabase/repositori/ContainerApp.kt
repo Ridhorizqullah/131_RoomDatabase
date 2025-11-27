d6353de33525e77f285c0eb083e6b649bb75fa78
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-27 16:28:10 +0700

    membuat kelas AplikasiSiswa untuk container aplikasi

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.roomdatabase.repositori
 
+import android.app.Application
 import android.content.Context
 import com.example.roomdatabase.room.DatabaseSiswa
 
@@ -10,6 +11,18 @@ interface ContainerApp{
 class ContainerDataApp(private val context: Context):
         ContainerApp {
     override val repositoriSiswa: RepositoriSiswa by lazy {
-        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application(){
+
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
     }
 }
