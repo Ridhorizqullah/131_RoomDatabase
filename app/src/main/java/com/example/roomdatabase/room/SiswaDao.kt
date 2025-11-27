1d82149cc0350cf5954138073cae3d517051cef0
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-27 16:00:30 +0700

    Membuat SiswaDao dan fungsinya

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -1,3 +1,18 @@
 package com.example.roomdatabase.room
 
-annotation class SiswaDao()
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface SiswaDao {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>
+
+     @Insert(onConflict = OnConflictStrategy.IGNORE)
+     suspend fun insert(siswa: Siswa)
+
+}
