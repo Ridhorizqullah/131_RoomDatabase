41862e1037bbfb89d02f1cd985891aa1e4cbf7e2
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-27 16:49:40 +0700

    membuat data class DetailSiswa dan fungsi toSiswa

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,2 +1,50 @@
 package com.example.roomdatabase.viewmodel
 
+import com.example.roomdatabase.room.Siswa
+
+class EntryViewModel {
+}
+
+
+
+
+
+
+
+
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
