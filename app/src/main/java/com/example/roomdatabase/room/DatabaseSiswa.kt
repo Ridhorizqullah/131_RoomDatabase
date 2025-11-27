583159f559ea61796386236bb941e6a08896b095
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-27 16:08:11 +0700

    Membuat database untuk data siswa

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -1,3 +1,24 @@
 package com.example.roomdatabase.room
 
-data class DatabaseSiswa()
+import android.content.Context
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+
+    abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return Instance ?: synchronized(this) {
+
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface SiswaDao {
     @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
-    fun getAllSiswa(): Flow<List<Siswa>
+    fun getAllSiswa(): Flow<List<Siswa>>
 
      @Insert(onConflict = OnConflictStrategy.IGNORE)
      suspend fun insert(siswa: Siswa)
