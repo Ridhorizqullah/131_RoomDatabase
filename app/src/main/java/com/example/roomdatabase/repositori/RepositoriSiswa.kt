8caad8119d7e5cfcc108f1900848bd16cc871312
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-27 16:17:04 +0700

    Membuat repositori siswa

---
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase.repositori
+
+
+import com.example.roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+
+interface RepositoriSiswa{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+
+}
+
+class
