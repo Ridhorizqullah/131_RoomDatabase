7cc0d11416aca41f3df5441fa1425690945d7dcb
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-12-04 15:53:02 +0700

    Menambahkan fungsi update pada SiswaDao

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 
@@ -21,6 +22,10 @@ interface SiswaDao {
 
      @Delete
      suspend fun delete(siswa: Siswa)
+
+     @Update
+     suspend fun update(siswa: Siswa)
+
 
 
 }
